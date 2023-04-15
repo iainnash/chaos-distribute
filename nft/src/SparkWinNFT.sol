45e5d959935ebd10f9b529afd0d7dec8cbb0189d
Iain <iain@zora.co>
2023-04-16 08:10:42 +0900

    add nft

---
+++ nft/src/SparkWinNFT.sol
@@ -0,0 +1,40 @@
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
+
+contract SparkWinNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address allowedMinter;
+
+    constructor(address _allowedMinter) ERC721("TestThing", "TSTTNG") {
+        allowedMinter = _allowedMinter;
+    }
+
+    /**
+     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
+     * by default, can be overridden in child contracts.
+     */
+    function _baseURI() internal override view virtual returns (string memory) {
+        return "https://chaos-distribute.vercel.app/nft/";
+    }
+
+    function mint(address recipient)
+        public
+        returns (uint256)
+    {
+        if (msg.sender != allowedMinter) {
+            revert("not allowed");
+        }
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
