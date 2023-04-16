72554d091597dab3dcbc31334d8b56dc1495fd5f
Iain <iain@zora.co>
2023-04-16 09:01:30 +0900

    txn history

--- client/src/components/TxnDisplay.tsx
+++ client/src/components/TxnDisplay.tsx
@@ -2,14 +2,14 @@ import { utils } from "ethers";
 import { CHAIN_FE } from "../constants/chains";
 
 export const TxnDisplay = ({ txn }: { txn: any }) =>
-  txn && txn.txn ? (
+  txn && txn.dataResp ? (
     <li>
-      <strong>{utils.formatEther(txn.txn.value.hex)}</strong>{" "}
-      {CHAIN_FE[txn.random.chain]?.currency} was sent to {txn.txn.to}
+      {/* <strong>{utils.formatEther(txn.dataResp.value.hex)}</strong>{" "} */}
+      {CHAIN_FE[txn.random.chain]?.currency} was sent to {txn.dataResp.to}
       {" // "}{" "}
       <a
         className="underline"
-        href={`https://${CHAIN_FE[txn.random.chain].scan}/tx/${txn.txn.hash}`}
+        href={`https://${CHAIN_FE[txn.random.chain].scan}/tx/${txn.dataResp.transactionHash}`}
       >
         view txn
       </a>
